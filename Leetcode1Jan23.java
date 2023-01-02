bffa2d2030a405d5dd98009b92c73172805a1fc3
Gopal Jain <47852634+gopaljain1st@users.noreply.github.com>
2023-01-02 16:32:03 +0530

    Update Leetcode1Jan23.java

--- Leetcode1Jan23.java
+++ Leetcode1Jan23.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        Map<Character, String> patternWords = new HashMap(pattern.length());
+        String[] words = s.split(" ");
+        int i = 0;
+        if(words.length != pattern.length())
+            return false;
+        for (String word : words) {
+            if (!patternWords.containsKey(pattern.charAt(i)) && !patternWords.containsValue(word))
+                patternWords.put(pattern.charAt(i), word);
+            i++;
+        }
+        for (i = 0; i < pattern.length(); i++) {
+            if (!words[i].equals(patternWords.get(pattern.charAt(i))))
+                return false;
+        }
+        return true;
+    }
+}
