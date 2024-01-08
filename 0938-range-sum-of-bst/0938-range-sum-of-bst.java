f2fa5bd5aa5a5f6f2b2cad654d0880eac5945b8f
Gopal Jain <47852634+gopaljain1st@users.noreply.github.com>
2024-01-08 23:29:02 +0530

    Time: 0 ms (100.00%), Space: 51 MB (23.62%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -18,12 +18,15 @@ class Solution {
        if (root == null) {
             return 0;
         }
-
-        int currentVal = (root.val >= low && root.val <= high) ? root.val : 0;
-
-        int leftSum = rangeSumBST(root.left, low, high);
-        int rightSum = rangeSumBST(root.right, low, high);
-
-        return currentVal + leftSum + rightSum;    
+      
+        if (low <= root.val && root.val <= high) {
+            return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+        } 
+        else if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        } 
+        else {
+            return rangeSumBST(root.left, low, high);
+        }    
     }
 }
