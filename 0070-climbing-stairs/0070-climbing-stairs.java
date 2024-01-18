974d4ef00b4bb4f08652d893c69d1bfe97935e5b
Gopal Jain <47852634+gopaljain1st@users.noreply.github.com>
2024-01-18 23:31:20 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (12.58%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        return countWays(n);
+    }
+    static int countWays(int n)
+    {
+        int prev = 1;
+        int prev2 = 1;
+        for (int i = 2; i <= n; i++) {
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+}
