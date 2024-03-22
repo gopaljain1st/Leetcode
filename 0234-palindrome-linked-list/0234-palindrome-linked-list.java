6cf32a8e5b482f805fdf9fc7716bf2851d48013b
Gopal Jain <47852634+gopaljain1st@users.noreply.github.com>
2024-03-22 18:41:11 +0530

    Time: 14 ms (15.21%), Space: 58.4 MB (80.89%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,17 +9,20 @@
  * }
  */
 class Solution {
-    ListNode ref;
     public boolean isPalindrome(ListNode head) {
-        ref = head;        
-        return check(head);
-    }
-    
-    public boolean check(ListNode node){
-        if(node == null) return true;
-        boolean ans = check(node.next);
-        boolean isEqual = (ref.val == node.val)? true : false; 
-        ref = ref.next;
-        return ans && isEqual;
+       ListNode temp = head;
+        Stack stack = new Stack();
+        while (temp != null) {
+            stack.push(temp.val);
+            temp = temp.next;
+        }
+        
+        while (head != null) {
+            if (head.val != (int)stack.pop()) {
+                return false;
+            }
+            head = head.next;
+        }
+        return true;
     }
 }
